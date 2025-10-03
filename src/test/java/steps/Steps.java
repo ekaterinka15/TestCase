dab0be5b645b8832dc5e943ab6596e66ed59e996
Ekaterina_Chechel <ekaterinka.15@yandex.ru>
2025-10-03 18:56:54 +0300

    обновление кода для Jenkins

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Assertions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.RegistrationPage;
@@ -44,7 +45,11 @@ public class Steps {
         String browser = properties.getProperty("type.browser");
         String selenoidUrl = properties.getProperty("selenoid.url");
         if ("remote".equalsIgnoreCase(driverType)) {
-
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--headless=new");
             DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
             Map<String, Object> selenoidOptions = new HashMap<>();
             desiredCapabilities.setBrowserName(browser);
