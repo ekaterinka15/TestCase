86857aad6705fb8160d9e6fab9c055585f6a6364
Ekaterina_Chechel <ekaterinka.15@yandex.ru>
2025-10-06 19:20:15 +0300

    обновление кода для Jenkins

--- src/test/java/runner/CucumberTest.java
+++ src/test/java/runner/CucumberTest.java
@@ -7,6 +7,6 @@ import org.junit.platform.suite.api.*;
 @SelectPackages("features")
 @ConfigurationParameter(key = "cucumber.plugin", value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 @ConfigurationParameter(key = "cucumber.glue", value = "steps")
-@ConfigurationParameter(key = "cucumber.tags",value = "${cucumberTag}")
+
 
 public class CucumberTest {}
